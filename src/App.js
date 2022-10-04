62bf80d3b194c6c54f11b96f1592456274d30cfa
tjrenffl8 <tjrenffl8@naver.com>
2022-10-04 13:02:07 +0900

    2022. 10. 04. Footer 생성

--- src/App.js
+++ src/App.js
@@ -2,27 +2,36 @@ import "./App.css";
 import "./css/Main.css";
 import "./css/Sign.css";
 import "./css/Bar.css";
-import { Bar } from "./pages/Bar";
+import "./css/Cmm.css";
+import { Bar, SideBar } from "./pages/Bar";
 import Main from "./pages/Main";
 import Login from "./pages/Sign/Login";
+import Footer from "./pages/Footer";
+import {List1, List2, List3, List4} from "./pages/List/List";
 
 import { Routes, Route, useLocation } from "react-router-dom";
 
 export default function App() {
   // useLocation 정보
   const location = useLocation().pathname;
-  console.log();
+
   return (
     <>
       {/* 지금 Url이 Login 페이지이면 Bar 안 나옴 */}
-      {location !== "/" ? <Bar /> : null}
-
+      {location !== "/" ? <Bar />  : null}
+      {location !== "/" ? <SideBar />  : null}
+      
       <div>
         <Routes>
           <Route path="/" element={<Login />}></Route>
           <Route path="/main" element={<Main />}></Route>
+          <Route path="/list1" element={<List1 />}></Route>
+          <Route path="/list2" element={<List2 />}></Route>
+          <Route path="/list3" element={<List3 />}></Route>
+          <Route path="/list4" element={<List4 />}></Route>
         </Routes>
       </div>
+      {location !== "/" ? <Footer /> : null}
     </>
   );
 }
