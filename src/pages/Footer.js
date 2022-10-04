91eb65cc4b6672a74d42d511db50f21ace11e582
tjrenffl8 <tjrenffl8@naver.com>
2022-10-04 14:25:14 +0900

    2022. 10. 04. footer github link 추가

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,10 +1,16 @@
 // Blog Footer 화면
 
+import {
+  faGithub
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Footer() {
   return (
     <>
       <div className="footer">
-        <a>© 2022 - copyright Seotjuu</a>
+        <a>Copyright ⓒ 2022 Seotjuu All rights reserved.</a>&emsp;
+        <a href="https://github.com/Seotjuu" color="black"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
       </div>
     </>
   );
