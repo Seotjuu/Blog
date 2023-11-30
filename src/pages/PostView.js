2969a4e20d496a700c8bdd1fe79439bfe12aff8d
Seotjuu <79149336+tjrenffl8@users.noreply.github.com>
2023-11-30 23:09:09 +0900

    [feat] firebase 사진 추가   - 스토리지 파일 업로드 다운로드 완료

--- src/pages/PostView.js
+++ src/pages/PostView.js
@@ -1,43 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import { useSearchParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 import ContentHeader from "../Component/ContentHeader";
 
-import { db } from '../firebase';
-import { doc, getDoc } from 'firebase/firestore';
+import { db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 import { Col, Container, Form, Row } from "react-bootstrap";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 const PostView = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const postId = searchParams.get('id')
+  const postId = searchParams.get("id");
 
   // post 컬랙션에 해당 문서 데이터
   const [postData, setPostData] = useState([]);
 
   // Firebase 게시물 데이터 가져오기
   const postEvent = async () => {
-  // 컬랙션 내에 문서의 ID에 맞는 데이터 저장
+    // 컬랙션 내에 문서의 ID에 맞는 데이터 저장
     const docRef = doc(db, "post", postId);
     const docSnap = await getDoc(docRef);
 
-    setPostData(docSnap.data())
-  }
-  useEffect(()=>{
+    setPostData(docSnap.data());
+  };
+  useEffect(() => {
     postEvent();
-  }, [])
+  }, []);
 
   return (
     <>
       <Container>
-        <ContentHeader title={postData.title} sub={postData.sub} author={postData.author} date={postData.date} id={postData.id}/>
-        
+        <ContentHeader
+          title={postData.title}
+          sub={postData.sub}
+          author={postData.author}
+          date={postData.date}
+          id={postData.id}
+          url={postData.url}
+        />
         포스트입니다.
-
       </Container>
     </>
   );
-}
+};
 
 export default PostView;
